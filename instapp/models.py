4f6d193e2ba8ebde167d27ce84236853e031a0f2
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-02-08 14:51:27 +0300

    create image and profile models

--- instapp/models.py
+++ instapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=100)
+    caption = models.TextField()
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    comment = models.CharField(max_length=140)
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    
+    
+    def __str__(self):
+        return self.name
+    
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='images/')
+    bio = models.CharField(max_length=180)
